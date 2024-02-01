1703f3dbcd35db7f10fc1c25f981142cf64fa71c
doanvinhphu <phudvse171839@fpt.edu.vn>
2024-02-01 14:14:08 +0700

    Hotfix Filter API key (add Swagger whitelist to API Key Filter), Fix Email, Phone Validator.

--- src/main/java/com/example/fams/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/fams/config/GlobalExceptionHandler.java
@@ -1,4 +1,6 @@
 package com.example.fams.config;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -7,10 +9,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -22,4 +23,21 @@ public class GlobalExceptionHandler {
         });
         return ResponseUtil.error(String.valueOf(errorMessage),"Bad request",HttpStatus.BAD_REQUEST);
     }
+
+    // ! Lộc add thêm vào ngày 01/02/2024
+//    @ExceptionHandler(ConstraintViolationException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException e) {
+//        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+//        List<String> errorMessages = violations.stream()
+//                .map(violation -> String.format("%s %s", violation.getPropertyPath(), violation.getMessage()))
+//                .collect(Collectors.toList());
+//
+//        // Join error messages for a detailed error message
+//        String detailedErrorMessage = String.join(", ", errorMessages);
+//        // Use the first error message as a user-friendly message
+//
+//        // Return a ResponseEntity with an error using ResponseUtil
+//        return ResponseUtil.error(detailedErrorMessage, "Bad request", HttpStatus.BAD_REQUEST);
+//    }
 }
