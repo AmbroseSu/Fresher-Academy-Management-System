3ccdfb21c55469395c20d5287e328f61725293f3
doanvinhphu <phudvse171839@fpt.edu.vn>
2024-02-22 13:11:24 +0700

    fix pageable for findAllBy()

--- src/main/java/com/example/fams/repository/LearningObjectiveRepository.java
+++ src/main/java/com/example/fams/repository/LearningObjectiveRepository.java
@@ -1,6 +1,7 @@
 package com.example.fams.repository;
 
 import com.example.fams.entities.LearningObjective;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,9 @@ import java.util.List;
 
 @Repository
 public interface LearningObjectiveRepository extends JpaRepository<LearningObjective, String> {
-    List<LearningObjective> findByStatusIsTrue();
+    List<LearningObjective> findByStatusIsTrue(Pageable pageable);
+    List<LearningObjective> findAllBy(Pageable pageable);
+    Long countByStatusIsTrue();
     LearningObjective findById(Long id);
     LearningObjective findByStatusIsTrueAndCode(String code);
     LearningObjective findByStatusIsTrueAndId(Long id);
