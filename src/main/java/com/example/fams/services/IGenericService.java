04eeb0248b22f1a55d80052f16a911510867b82f
PhCh2k3 <phongdcse171753@fpt.edu.vn>
2024-02-22 09:04:51 +0700

    commit before pull 2

---
+++ src/main/java/com/example/fams/services/IGenericService.java
@@ -0,0 +1,15 @@
+package com.example.fams.services;
+
+import org.springframework.http.ResponseEntity;
+
+public interface IGenericService<T> {
+    ResponseEntity<?> findById(Long id);
+
+    ResponseEntity<?> findAllByStatusTrue(int page, int limit);
+    ResponseEntity<?> findAll(int page, int limit);
+
+    ResponseEntity<?> save(T t);
+
+    ResponseEntity<?> changeStatus(Long id);
+
+}
