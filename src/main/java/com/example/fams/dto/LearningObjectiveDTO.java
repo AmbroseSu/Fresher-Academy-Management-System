bfa072b33f2284021b6331625f2f93214e843c56
doanvinhphu <phudvse171839@fpt.edu.vn>
2024-03-01 03:45:57 +0700

    fix validation utils

--- src/main/java/com/example/fams/dto/LearningObjectiveDTO.java
+++ src/main/java/com/example/fams/dto/LearningObjectiveDTO.java
@@ -1,10 +1,14 @@
 package com.example.fams.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+
 
 @Data
 @AllArgsConstructor
@@ -12,12 +16,16 @@ import java.util.List;
 public class LearningObjectiveDTO {
     private Long id;
 
+    @NotBlank(message = "Code must not be blank")
     private String code;
 
+    @NotBlank(message = "Name must not be blank")
     private String name;
 
+    @NotNull(message = "Type must not be null")
     private Integer type;
 
+    @NotBlank(message = "Description must not be blank")
     private String description;
 
     private Boolean status;
