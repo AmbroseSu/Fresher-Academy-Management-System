938615099578765f2aac96112dde55207b859356
doanvinhphu <phudvse171839@fpt.edu.vn>
2024-03-14 01:10:53 +0700

    Add Authorities for Controller, Upload Syllabus and Set User Avatar

---
+++ src/main/java/com/example/fams/entities/enums/DeliveryType.java
@@ -0,0 +1,10 @@
+package com.example.fams.entities.enums;
+
+public enum DeliveryType {
+    Assignment_Lab,
+    Concept_Lecture,
+    Guide_Review,
+    Test_Quiz,
+    Exam,
+    Seminar_Workshop
+}
